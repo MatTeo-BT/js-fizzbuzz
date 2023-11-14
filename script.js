fe9b896f0174899092d6f8c11b792a5df3a61280
MatTeo-BT <aboutmet@gmail.com>
2023-11-14 16:29:42 +0100

    implementati multipli di 5 e di entrambi (15)

--- script.js
+++ script.js
@@ -2,7 +2,15 @@
 for(let i = 1; i < 100; i++){
     console.log(i);
     if (i % 3){
-        const multiploDi3 = Fizz;
-        console.log(Fizz);
+        const multiploDi3 = `Fizz`;
+        console.log(`Fizz`);
+    }
+    if (i % 5){
+        const multiploDi5 = `Buzz`;
+        console.log(`Buzz`);
+    }
+    if (i % 15){
+        const multiploDiEntrambi = `FizzBuzz`;
+        console.log(`FizzBuzz`);
     }
 }
